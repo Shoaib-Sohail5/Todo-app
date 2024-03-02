36f8aa77db29ee8be4c0f6759776d43af65a9a5b
Shoaib Sohail <shoaibsohail.shaik@gmail.com>
2024-03-02 12:10:59 +0530

    added zod validation

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 const express = require('express')
+const { createTodo, updateTodo } = require('./types')
 const app = express()
 
 app.post('/todo', (req, res) => {
-    res.send('Hello World')
+    const createPayload = req.body;
+    const parsePayload =  createTodo.safeParse(createPayload)
+
+    if(!parsePayload.success) {
+        res.status(411).json({
+            msg: "you sent wrong inputs"
+        })
+        return
+    }
+
 })
 
 app.put('/completed', (req, res) => {
-    res.send('Hello World')
+    const updatePayload = updateTodo
+    const parsePayload = updateTodo.safeParse(updatePayload)
+
+    if(!parsePayload.success) {
+        res.status(411).json({
+            msg: "you sent wrong inputs"
+        })
+        return
+    }
+
 })
 
 app.get('/todos', (req, res) => {
